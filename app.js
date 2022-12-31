601f2693176415bb35379ea21ec92cfdf436972a
G4be34 <jimenezgabriel12@gmail.com>
2022-12-30 21:56:53 -0700

    Add styles for new divs and create function to make new divs

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-let hello = "Hello world";
-console.log(hello);
+const choice = 16;
+
+let columns = choice;
+let rows = choice;
+
+const grid = document.getElementById('grid-body');
+
+for (let i = 0; i < columns; i++) {
+    let column = document.createElement('div');
+    column.className = 'column';
+    for (let r = 0; r < rows; r++) {
+        let row = document.createElement('div');
+        row.className = 'row';
+        column.appendChild(row);
+    };
+    grid.appendChild(column);
+};
